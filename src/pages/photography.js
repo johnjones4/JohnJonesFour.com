cb63ba5b974e9348f8896c52cd15fc297c5199af
John Jones <johnjones4@gmail.com>
2020-01-29 21:18:59 -0500

    photo gallery redesign

--- src/pages/photography.js
+++ src/pages/photography.js
@@ -2,61 +2,83 @@ import React, {Component} from 'react'
 import Page from '../components/page'
 import { graphql } from 'gatsby'
 import './photography.scss'
+import _ from 'lodash'
+import {
+  Container,
+  Col,
+  Row,
+} from 'reactstrap'
 
 export default class Photography extends Component {
   constructor (props) {
     super(props)
-    
     this.state = {
-      selectedPhotos: {
-        a: 0,
-        b: null
-      },
-      activePhoto: 'a',
+      selectedPhoto: null
     }
-
-    this.photographyPhotos = React.createRef()
-
-    this.preloadedImages = []
   }
 
-  setNextPhoto (index) {
-    this.loadImages(index)
-    if (index < 0 || index >= this.props.data.allFlickrPhoto.edges.length) {
-      return
-    }
-    const nextPhoto = this.state.activePhoto === 'a' ? 'b' : 'a'
-    const newSelectedPhotos = {}
-    newSelectedPhotos[nextPhoto] = index
+  // setNextPhoto (index) {
+  //   this.loadImages(index)
+  //   if (index < 0 || index >= this.props.data.allFlickrPhoto.edges.length) {
+  //     return
+  //   }
+  //   const nextPhoto = this.state.activePhoto === 'a' ? 'b' : 'a'
+  //   const newSelectedPhotos = {}
+  //   newSelectedPhotos[nextPhoto] = index
+  //   this.setState({
+  //     selectedPhotos: Object.assign({}, this.state.selectedPhotos, newSelectedPhotos),
+  //     activePhoto: nextPhoto
+  //   })
+  //   const button = this.photographyPhotos.current.querySelectorAll('.photography-photo-button')[index]
+  //   this.photographyPhotos.current.scroll({
+  //     top: 0,
+  //     left: button.offsetLeft - (window.innerWidth / 2) + (button.offsetWidth / 2),
+  //     behavior: 'smooth'
+  //   })
+  // }
+
+  // loadImages (baseIndex) {
+  //   for (let i = Math.max(0, baseIndex - 10); i < Math.min(this.props.data.allFlickrPhoto.edges.length, baseIndex + 10); i++) {
+  //     const url = this.props.data.allFlickrPhoto.edges[i].node.url_l
+  //     if (this.preloadedImages.indexOf(url) <= 0) {
+  //       const image = new Image()
+  //       image.src = url
+  //       this.preloadedImages.push(url)
+  //     }
+  //   }
+  // }
+
+  selectPhoto (selectedPhoto) {
     this.setState({
-      selectedPhotos: Object.assign({}, this.state.selectedPhotos, newSelectedPhotos),
-      activePhoto: nextPhoto
+      selectedPhoto
     })
-    const button = this.photographyPhotos.current.querySelectorAll('.photography-photo-button')[index]
-    this.photographyPhotos.current.scroll({
-      top: 0,
-      left: button.offsetLeft - (window.innerWidth / 2) + (button.offsetWidth / 2),
-      behavior: 'smooth'
-    })
-  }
-
-  loadImages (baseIndex) {
-    for (let i = Math.max(0, baseIndex - 10); i < Math.min(this.props.data.allFlickrPhoto.edges.length, baseIndex + 10); i++) {
-      const url = this.props.data.allFlickrPhoto.edges[i].node.url_l
-      if (this.preloadedImages.indexOf(url) <= 0) {
-        const image = new Image()
-        image.src = url
-        this.preloadedImages.push(url)
-      }
-    }
   }
 
   render () {
-    const selectedPhotoA = this.props.data.allFlickrPhoto.edges[this.state.selectedPhotos.a].node
-    const selectedPhotoB = this.state.selectedPhotos.b ? this.props.data.allFlickrPhoto.edges[this.state.selectedPhotos.b].node : null
     return (
       <Page slug='photography' title='Photography'>
-        <div className='photography-selected-photo-wrapper'>
+        <Container fluid>
+          <Row>
+            {
+              this.props.data.allFlickrPhoto.edges.map((row, i) => {
+                return (
+                  <Col lg='1' md='2' sm='3' xs='6'>
+                    <button key={i} className='photography-photo-button' onClick={() => this.selectPhoto(this.props.data.allFlickrPhoto.edges[i])} style={{backgroundImage: 'url(' + row.node.url_z + ')'}}>
+                      <span className='sr-only'>{row.node.title}</span>
+                    </button>
+                  </Col>
+                )
+              })
+            }
+          </Row>
+        </Container>
+        { this.state.selectedPhoto && (
+          <div class='selected-photo-wrapper' onClick={() => this.selectPhoto(null)}>
+            <button className='selected-photo-close' onClick={() => this.selectPhoto(null)}>&times;</button>
+            <div class='selected-photo' style={{backgroundImage: 'url(' + this.state.selectedPhoto.node.url_z + ')'}}></div>
+          </div>
+        )}
+        {/* <div className='photography-selected-photo-wrapper'>
           <button onClick={() => this.setNextPhoto(this.state.selectedPhotos[this.state.activePhoto] - 1)} className={['photography-page-button','photography-page-button-previous','photography-page-button-' + (this.state.selectedPhotos[this.state.activePhoto] > 0 ? 'active' : 'inactive')].join(' ')}>
             <i className='fas fa-angle-left'>
               <span className='sr-only'>Page Left</span>
@@ -89,7 +111,7 @@ export default class Photography extends Component {
               )
             })
           }
-        </div>
+        </div> */}
       </Page>
     )
   }
