a88a697a387eb7cd1591382a8cffdc8ee5e0a5b1
John Jones <johnjones4@gmail.com>
2020-09-05 07:23:56 -0400

    New photo gallery

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,15 +40,6 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-source-flickr',
-      options: {
-        api_key: process.env.FLICKR_API_KEY,
-        user_id: '28315761@N06',
-        method: 'flickr.photosets.getPhotos',
-        photoset_id: '72157712896159468'
-      }
-    },
-    {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: 'UA-23892666-1',
--- src/pages/photography.js
+++ src/pages/photography.js
@@ -4,65 +4,119 @@ import { graphql } from 'gatsby'
 import './photography.scss'
 import {
   Container,
-  Col,
   Row,
+  Col
 } from 'reactstrap'
 
 export default class Photography extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      selectedPhoto: null
+      selectedPhotoIndex: 0,
+      category: null,
+      categories: []
     }
-  }
-
-  selectPhoto (selectedPhoto) {
-    this.setState({
-      selectedPhoto
+    this.props.data.allPhotographyJson.edges.forEach(({ node }) => {
+      const img = new Image()
+      img.src = `/photography/${node.filename}`
     })
   }
 
+  static getDerivedStateFromProps(props, state) {
+    if (!state.categories || state.categories.length === 0) {
+      state.categories = []
+      props.data.allPhotographyJson.edges.forEach(({ node }) => {
+        node.categories.forEach(category => {
+          if (state.categories.indexOf(category) < 0) {
+            state.categories.push(category)
+          }
+        })
+      })
+      state.categories.sort()
+      return state
+    }
+    return null
+  }
+
+  getAvailablePhotos () {
+    if (!this.state.category) {
+      return this.props.data.allPhotographyJson.edges.map(({ node }) => node)
+    }
+    return this.props.data.allPhotographyJson.edges.filter(({ node }) => node.categories.indexOf(this.state.category) >= 0).map(({ node }) => node)
+  }
+
   render () {
+    const photos = this.getAvailablePhotos()
+    const index = this.state.selectedPhotoIndex
+    if (index >= photos.length) {
+      return null
+    }
+    const photo = photos[index]
+    const next = index < photos.length - 1 ? photos[index + 1] : null
+    const previous = index > 0 ? photos[index - 1] : null
     return (
       <Page slug='photography' title='Photography'>
         <Container fluid>
-          <Row>
-            {
-              this.props.data.allFlickrPhoto.edges.map((row, i) => {
-                return (
-                  <Col lg='2' md='2' sm='3' xs='6' key={i}>
-                    <button key={i} className='photography-photo-button' onClick={() => this.selectPhoto(this.props.data.allFlickrPhoto.edges[i])} style={{backgroundImage: 'url(' + row.node.url_z + ')'}}>
-                      <span className='sr-only'>{row.node.title}</span>
-                    </button>
-                  </Col>
-                )
-              })
-            }
+          <Row className='photography-gallery'>
+            <Col sm={{ size: 12, order: 1 }} md={{ size: 10, order: 2 }} className='photography-col' >
+              <img className='photography-photo' src={`/photography/${photo.filename}`} alt={photo.caption} />
+            </Col>
+            <Col xs={{ size: 6, order: 2 }} sm={{ size: 6, order: 2 }}  md={{ size: 1, order: 1 }} className='photography-col'>
+              { previous && (<button
+                className='photography-button photography-button-previous'
+                onClick={() => this.setState({selectedPhotoIndex: this.state.selectedPhotoIndex - 1})} 
+                style={{
+                  backgroundImage: previous ? `url('/photography/${previous.filename}')` : null
+                }}
+              >
+                Previous
+              </button>) }
+            </Col>
+            <Col xs={{ size: 6, order: 2 }}  sm={{ size: 6, order: 2 }}  md={{ size: 1, order: 3 }} className='photography-col'>
+              {next && (<button
+                className='photography-button photography-button-next'
+                onClick={() => this.setState({selectedPhotoIndex: this.state.selectedPhotoIndex + 1})} 
+                style={{
+                  backgroundImage: next ? `url('/photography/${next.filename}')` : null
+                }}
+              >
+                Next
+              </button>) }
+            </Col>
+            <div className='photography-counter'>
+              { index + 1 } / { photos.length }
+            </div>
           </Row>
-        </Container>
-        { this.state.selectedPhoto && (
-          <div class='selected-photo-wrapper' onClick={() => this.selectPhoto(null)}>
-            <button className='selected-photo-close' onClick={() => this.selectPhoto(null)}>&times;</button>
-            <div class='selected-photo' style={{backgroundImage: 'url(' + this.state.selectedPhoto.node.url_l + ')'}}></div>
+          <div className='photography-categories'>
+            <button className={`photography-category${!this.state.category ? ' active' : ''}`} onClick={() => this.setState({
+              category: null,
+              selectedPhotoIndex: 0
+            })}>
+              All
+            </button>
+            { this.state.categories.map((category) => (
+              <button className={`photography-category${category === this.state.category ? ' active' : ''}`} key={category} onClick={() => this.setState({
+                category,
+                selectedPhotoIndex: 0
+              })}>
+                {category}
+              </button>
+            ))}
           </div>
-        )}``
+        </Container>
       </Page>
     )
   }
 }
 
 export const query = graphql`
-  query PhotoQuery {
-    allFlickrPhoto(limit:500) {
+  query {
+    allPhotographyJson {
       edges {
         node {
-          id
-          title
-          tags
-          url_l
-          url_z
-          width_l
-          height_l
+          caption
+          categories
+          filename
         }
       }
     }
